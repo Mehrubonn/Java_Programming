fa857f5198b98b4569cee0552afcce32dd6334dc
Mehrubonn <123918520+Mehrubonn@users.noreply.github.com>
2023-05-06 04:52:21 -0400

    added area method

--- src/day36_polymorphism/Circle.java
+++ src/day36_polymorphism/Circle.java
@@ -4,4 +4,32 @@ public class Circle {
     private double radius;
     public final static double PI = 3.14;
 
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double area(){
+        return radius*radius*PI;
+    }
+
+    public double perimeter(){
+        return 2*radius*PI;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                "area=" + area() +
+                "perimeter="+perimeter() +
+                '}';
+    }
 }
